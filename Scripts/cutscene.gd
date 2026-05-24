39457487d1195b898c782a10e07a71f868c45ffa
MrMo8 <95354527+MrMo8@users.noreply.github.com>
2026-05-24 13:20:31 +0200

    Add datastructure for cutscenes, animations and timeline

---
+++ Scripts/cutscene.gd
@@ -0,0 +1,15 @@
+class_name Cutscene
+extends Node2D
+
+## Dokumentation
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#TODO load cutscene and animation data
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
