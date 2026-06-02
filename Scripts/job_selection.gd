fa61c41a46b7bee4b4ddc5ba44a0814f4d318bbc
MrMo8 <95354527+MrMo8@users.noreply.github.com>
2026-06-02 09:42:55 +0200

    Fix job selection not working

--- Scripts/job_selection.gd
+++ Scripts/job_selection.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#pass # Replace with function body.
+func _ready() -> void:
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,9 +11,12 @@ func _process(_delta: float) -> void:
 	var selected_carousel_node = $CarouselContainer.position_offset_node.get_child($CarouselContainer.selected_index)
 	print(selected_carousel_node.name)
 	print("test")
+
+
 func _on_left_pressed() -> void:
 	$CarouselContainer._left()
 	print("???")
+
 
 func _on_right_pressed() -> void:
 	$CarouselContainer._right()
