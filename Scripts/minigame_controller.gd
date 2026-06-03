ee1812871aa7e929d41af4a7aeb40c4e1ada1208
danil <danyil.lvov@tu-ilmenau.de>
2026-06-03 07:17:37 +0200

    Minigame Controller: added drag and drop minigame

--- Scripts/minigame_controller.gd
+++ Scripts/minigame_controller.gd
@@ -16,7 +16,7 @@ enum content_type {
 var minigames :={
 	  "0": {
 		"name": "Minigame1",
-		"questions_amount": 3,
+		"questions_amount": 4,
 		# TODO: add win/loose message
 		"questions": 
 			[{
@@ -114,6 +114,44 @@ var minigames :={
 						"content_type": content_type.NORMAL_QUESTION
 					}
 				}
+			},
+			{
+				"question": "Was sollte mit diesem Commit passieren?",
+				"question_type": minigame_type.DRAG_AND_DROP,
+				"answers_amount": 5,
+				"categories_amount": 3,
+				"categories": {
+					"1": "gut",
+					"2": "naja",
+					"3": "nein"
+				},
+				"answers": {
+					"0": {
+						"text": "Direkt mergen, ohne ihn weiter zu prüfen.",
+						"content_type": content_type.NORMAL_QUESTION,
+						"answer": "1" 
+					},
+					"1": {
+						"text": "Ablehnen, weil Tests deaktiviert wurden.",
+						"content_type": content_type.NORMAL_QUESTION,
+						"answer": "2" 
+					},
+					"2": {
+						"text": "Nur die Commit-Nachricht ändern und dann mergen.",
+						"content_type": content_type.NORMAL_QUESTION,
+						"answer": "3" 
+					},
+					"3": {
+						"text": "Ignorieren, weil es nur ein kleiner Hotfix ist.",
+						"content_type": content_type.NORMAL_QUESTION,
+						"answer": "3"
+					},
+					"4": {
+						"text": "Nur die Commit-Nachricht ändern und dann mergen.",
+						"content_type": content_type.NORMAL_QUESTION,
+						"answer": "2" 
+					}
+				}
 			}]
 		}
 	 
@@ -136,12 +174,22 @@ var ordering_res = {
 	"amount": 0,
 	"order": []
 }
+var drag_and_drop_res = {
+	"dragging": false,
+	"offset": Vector2.ZERO,
+	"start": Vector2.ZERO,
+	"categories": [],
+	"categories_amount": 0,
+	"answers_completed": 0,
+	"answers_amount": 0
+}
+
 @onready var minigame_container := $MinigameContent
 @onready var next_button := $Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	current_question = 2
+	current_question = 3
 	pass # Replace with function body.
 
 
@@ -167,6 +215,7 @@ func _draw_question(question: Dictionary) -> void:
 	# Question container and header
 	content_body = UIFactory.create_panel_container (Vector2(450, 600))
 	var question_text = UIFactory.create_label(question.get("question"))
+	
 	# adding extra content in header if available
 	if question.has("extra_content"):
 		var vbox = VBoxContainer.new()
@@ -227,8 +276,34 @@ func _draw_question(question: Dictionary) -> void:
 			# when confirm is pressed check for selected buttons if they match our answer if yes, call _answer_correct
 			pass	
 		minigame_type.DRAG_AND_DROP:
-		
-			pass
+			
+			for answer_key in question.get("answers"):
+				var answer = question["answers"][answer_key]
+				var root := PanelContainer.new()
+				root.custom_minimum_size = Vector2 (150, 50)
+				root.set_meta("category_id", answer["answer"])
+				content_body.get("content").add_child(root)
+				root.gui_input.connect(_handle_drag_and_drop.bind(root))
+				drag_and_drop_res["answers_amount"] += 1
+				
+
+			var footer = HBoxContainer.new()
+			footer.add_theme_constant_override("separation", margin_default)
+			content_body.get("footer").add_child(footer)
+
+			# Categories for Drag and Drop
+			drag_and_drop_res["categories_amount"] = question.get("categories_amount")
+			drag_and_drop_res["categories"].resize(drag_and_drop_res["categories_amount"])
+			var index = 0
+			for categorie in question.get("categories"):
+				var root := PanelContainer.new()
+				root.custom_minimum_size = Vector2 (150, 150)
+				#print(categorie)
+				root.set_meta("category_id", categorie)
+				footer.add_child(root)
+
+				drag_and_drop_res["categories"][index] = root
+				index += 1
 		minigame_type.ORDER:
 			var answers_amount = question.get("answers_amount")
 			var rows = HBoxContainer.new()
@@ -240,14 +315,12 @@ func _draw_question(question: Dictionary) -> void:
 			for i in answers_amount:
 				var root := PanelContainer.new()
 				root.custom_minimum_size = Vector2 (50, 50)
-				root.gui_input.connect(_handle_dragging.bind(root))
+				root.gui_input.connect(_handle_ordering.bind(root))
 				ordering_res.get("nodes")[i] = root
 				ordering_res.get("order")[i] = i
 				rows.add_child(root)
 			
 			content_body.get("content").add_child(rows)
-
-
 		minigame_type.CONNECT:
 			pass
 	
@@ -267,8 +340,56 @@ func _draw_content(content: Dictionary) -> Dictionary:
 
 # inputs handling
 
+func _handle_drag_and_drop(event: InputEvent, root: Control) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		# Started dragging
+		if event.pressed:
+			drag_and_drop_res["start"] = root.global_position
+			drag_and_drop_res["dragging"] = true
+			drag_and_drop_res["offset"] = root.get_global_mouse_position() - root.global_position
+		# Stopped dragging
+		else:
+			
+			var tween := root.create_tween()
+			drag_and_drop_res["dragging"] = false
 
-func _handle_dragging(event: InputEvent, root: Control) -> void:
+			# checking for intersection with any categorie
+			var best_category: Control = null
+			var best_overlap_ratio := 0.0
+			var min_overlap_ratio := 0.25
+			var answer_rect : Rect2 = root.get_global_rect()
+			var answer_area := answer_rect.size.x * answer_rect.size.y
+			for category in drag_and_drop_res["categories"]:
+				var category_rect: Rect2 = category.get_global_rect()
+
+				if answer_rect.intersects(category_rect):
+					var intersection: Rect2 = answer_rect.intersection(category_rect)
+					var overlap_area := intersection.size.x * intersection.size.y
+					var overlap_ratio := overlap_area / answer_area
+
+					if overlap_ratio > best_overlap_ratio and overlap_ratio >= min_overlap_ratio:
+						best_overlap_ratio = overlap_ratio
+						best_category = category
+			
+			if best_category == null:
+				tween.tween_property(root, "global_position", drag_and_drop_res["start"], 0.1)
+			elif best_category.get_meta("category_id") != root.get_meta("category_id"):
+				tween.tween_property(root, "global_position", drag_and_drop_res["start"], 0.1)
+				_shake_node(best_category)
+			else:
+				drag_and_drop_res["answers_completed"] += 1
+				if drag_and_drop_res["answers_completed"] == drag_and_drop_res["answers_amount"]:
+					_answer_correct()
+				var target = best_category.global_position + best_category.size / 2.0  
+				tween.tween_property(root, "global_position", target, 0.2)
+				tween.parallel().tween_property(root, "scale", Vector2.ZERO, 0.2)
+
+
+		
+	elif event is InputEventMouseMotion and drag_and_drop_res["dragging"]:
+		_drag_node_with_mouse(root, drag_and_drop_res["offset"])
+		
+func _handle_ordering(event: InputEvent, root: Control) -> void:
 	
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		# if started dragging
@@ -320,15 +441,31 @@ func _handle_dragging(event: InputEvent, root: Control) -> void:
 
 
 	elif event is InputEventMouseMotion and ordering_res["dragging"]:
-		var new_pos: Vector2 = root.get_global_mouse_position() - ordering_res["offset"]
-		
-		var viewport_size: Vector2 = root.get_viewport_rect().size
-		var root_size: Vector2 = root.size
+		_drag_node_with_mouse(root, ordering_res["offset"])
 
-		new_pos.x = clamp(new_pos.x, 0.0, viewport_size.x - root_size.x)
-		new_pos.y = clamp(new_pos.y, 0.0, viewport_size.y - root_size.y)
+func _shake_node(root: Control) -> void:
+	var start_pos := root.global_position
+	var shake_power := 8.0
+	var step_time := 0.04
 
-		root.global_position = new_pos
+	var tween := root.create_tween()
+
+	tween.tween_property(root, "global_position", start_pos + Vector2(shake_power, 0), step_time)
+	tween.tween_property(root, "global_position", start_pos + Vector2(-shake_power, 0), step_time)
+	tween.tween_property(root, "global_position", start_pos + Vector2(shake_power * 0.6, 0), step_time)
+	tween.tween_property(root, "global_position", start_pos + Vector2(-shake_power * 0.6, 0), step_time)
+	tween.tween_property(root, "global_position", start_pos, step_time)
+
+func _drag_node_with_mouse(root: Control, offset: Vector2) -> void:
+	var new_pos: Vector2 = root.get_global_mouse_position() - offset
+
+	var viewport_size: Vector2 = root.get_viewport_rect().size
+	var root_size: Vector2 = root.size
+
+	new_pos.x = clamp(new_pos.x, 0.0, viewport_size.x - root_size.x)
+	new_pos.y = clamp(new_pos.y, 0.0, viewport_size.y - root_size.y)
+
+	root.global_position = new_pos
 
 func _on_next_button_pressed() -> void:
 	next_button.visible = false
